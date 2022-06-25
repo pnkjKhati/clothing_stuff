8b5cb975106957ccf3ec167e0ef1f7aae3b40f4c
pnkjKhati <pankaj.khati.45@gamil.com>
2022-06-25 13:19:24 +0530

    added cart context and shopping icon

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -5,7 +5,7 @@ const ProductCard = ({product}) => {
     const {name,price,imageUrl} = product
     return(
         <div className='product-card-container'>
-            <img src={`${imageUrl}`}/>
+            <img src={`${imageUrl}`} alt={`${name}`}/>
             <div className='footer'>
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
--- src/routes/navigation/navigation.container.jsx
+++ src/routes/navigation/navigation.container.jsx
@@ -4,11 +4,13 @@ import {ReactComponent as Crwnsvg} from '../../assets/crown.svg';
 import { useContext } from "react";
 import { UserContext } from "../../contexts/user-context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart.icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component"
+import { CartContext } from "../../contexts/cart.component";
 
 const Navigation = () => {
   const {currentUser} = useContext(UserContext)
-
-  
+  const {isCartOpen} = useContext(CartContext)
 
     return(
       <>  
@@ -20,14 +22,17 @@ const Navigation = () => {
                 <Link className="nav-link" to="/shop">
                     SHOP
                 </Link>
-                {currentUser?
+                {currentUser
+                    ?
                     <span className="nav-link" onClick={signOutUser}>SIGN OUT</span>
                     :
                     <Link className="nav-link" to="/auth">
                         SIGN IN
                     </Link>
                 }
+                <CartIcon/>
             </div>
+            { isCartOpen && <CartDropdown/>}
         </div>
         <Outlet/>
       </>
